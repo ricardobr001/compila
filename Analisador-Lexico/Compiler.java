80ecf46d3033edc81f4e31d9e48bdadd2ea6df38
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-03-29 16:01:50 -0300

    Mais alguns métodos do compiler

--- Analisador-Lexico/Compiler.java
+++ Analisador-Lexico/Compiler.java
@@ -208,7 +208,7 @@ public class Compiler {
 	public void var_decl_tail(){
 		var_decl();
 
-		while (lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT){	// Mesma duvida do método string_decl_tail
+		if (lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT){
 			var_decl_tail();
 		}
 	}
@@ -302,5 +302,214 @@ public class Compiler {
 		stmt_lis();
 	}
 
+	// stmt_list -> stmt stmt_tail | empty
+	public void stmt_list(){
+		if (lexer.token == Symbol.IDENTIFIER || lexer.token == Symbol.READ || lexer.token == Symbol.WRITE || lexer.token == Symbol.RETURN || lexer.token == Symbol.IF || lexer.token == Symbol.FOR){
+			stmt();
+			stmt_tail();
+		}
+	}
 
-}
+	// stmt_tail -> stmt stmt_tail | empty
+	public void stmt_tail(){
+		if (lexer.token == Symbol.IDENTIFIER || lexer.token == Symbol.READ || lexer.token == Symbol.WRITE || lexer.token == Symbol.RETURN || lexer.token == Symbol.IF || lexer.token == Symbol.FOR){
+			stmt();
+			stmt_tail();
+		}
+	}
+
+	// stmt -> id assign_stmt | read_stmt | write_stmt | return_stmt | if_stmt | for_stmt | id call_expr ;
+	public void stmt(){
+		if (lexer.token == Symbol.IDENTIFIER){
+			id();
+			if (lexer.token == Symbol.LPAR)
+				call_expr();	//call_expr -> id ( {expr_list} )
+			else if (lexer.token == Symbol.ASSIGN)
+				assign_stmt();	//assign_stmt -> assign_expr ;
+								//assign_expr -> id := expr
+			else
+				error.signal("Faltou := ou (")
+		}
+		//COLOCAR NO CASO DE TESTE
+
+		else if (lexer.token == Symbol.READ)
+		 	read_stmt();
+
+		else if (lexer.token == Symbol.WRITE)
+		 	write_stmt();
+
+		else if (lexer.token == Symbol.RETURN)
+		 	return_stmt();
+
+		else if (lexer.token == Symbol.IF)
+		 	if_stmt();
+
+		else if (lexer.token == Symbol.FOR)
+			for_stmt();
+	}
+
+	/* Basic Statements */
+	// assign_stmt -> assign_expr ;
+	public void assign_stmt(){
+		assign_expr();
+		if (lexer.token != Symbol.SEMICOLON)
+			error.signal("Faltou ;");
+		lexer.nextToken();
+	}
+
+	// assign_expr -> := expr
+	public void assign_expr(){
+		if (lexer.token != Symbol.ASSIGN)
+			error.signal("Faltou :=");
+		lexer.nextToken();
+		expr();
+	}
+
+	// read_stmt -> READ ( id_list );
+	public void read_stmt(){
+		if (lexer.token != Symbol.READ)
+			error.signal("Faltou READ");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.LPAR)
+			error.signal("Faltou (");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.IDENTIFIER)
+			error.signal("Faltou variavel");
+		id_list();
+
+		if (lexer.token != Symbol.RPAR)
+			error.signal("Faltou )");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.SEMICOLON)
+			error.signal("Faltou ;");
+		lexer.nextToken();
+	}
+
+	// write_stmt -> WRITE ( id_list );
+	public void write_stmt(){
+		if (lexer.token != Symbol.WRITE)
+			error.signal("Faltou WRITE");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.LPAR)
+			error.signal("Faltou (");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.IDENTIFIER)
+			error.signal("Faltou variavel");
+		id_list();
+
+		if (lexer.token != Symbol.RPAR)
+			error.signal("Faltou )");
+		lexer.nextToken();
+
+		if (lexer.token != Symbol.SEMICOLON)
+			error.signal("Faltou ;");
+		lexer.nextToken();
+	}
+
+	// return_stmt -> RETURN expr ;
+	public void return_stmt(){
+		if (lexer.token != Symbol.RETURN)
+			error.signal("Faltou RETURN");
+		lexer.nextToken();
+		expr();
+
+		if (lexer.token != Symbol.SEMICOLON)
+			error.signal("Faltou ;");
+		lexer.nextToken();
+	}
+
+
+	/* Expressions */
+
+	// expr -> factor expr_tail
+	public void expr(){
+		factor();
+		expr_tail();
+	}
+
+	// expr_tail -> addop factor expr_tail | empty
+	public void expr_tail(){
+		if (lexer.token == Symbol.PLUS || lexer.token == Symbol.MINUS){
+			addop();
+			factor();
+			expr_tail();
+		}
+	}
+
+	// factor -> postfix_expr factor_tail
+	public void factor(){
+
+	}
+
+	// factor_tail -> mulop postfix_expr factor_tail | empty
+	public void factor_tail(){
+
+	}
+
+	// postfix_expr -> primary | call_expr
+	public void postfix_expr(){
+
+	}
+
+	// call_expr -> ( {expr_list} )
+	public void call_expr(){
+
+	}
+
+	// expr_list -> expr expr_list_tail
+	public void expr_list(){
+
+	}
+
+	// expr_list_tail -> , expr expr_list_tail | empty
+	public void expr_list_tail(){
+
+	}
+
+	// primary -> (expr) | id | INTLITERAL | FLOATLITERAL
+	public void primary(){
+
+	}
+
+	// addop -> + | -
+	public void addop(){
+
+	}
+
+	// mulop -> * | /
+	public void mulop(){
+
+	}
+
+
+	/* Complex Statements and Condition */
+
+	// if_stmt -> IF ( cond ) THEN stmt_list else_part ENDIF
+	public void if_stmt(){
+
+	}
+
+	// else_part -> ELSE stmt_list | empty
+	public void else_part(){
+
+	}
+
+	// cond -> expr compop expr
+	public void cond(){
+
+	}
+
+	// compop -> < | > | =
+	public void compop(){
+
+	}
+
+	// for_stmt -> FOR ({assign_expr}; {cond}; {assign_expr}) stmt_list ENDFOR
+	public void for_stmt(){
+
+	}}
--- Analisador-Lexico/Lexer/Symbol.java
+++ Analisador-Lexico/Lexer/Symbol.java
@@ -1,35 +1,48 @@
 package Lexer;
 
 public enum Symbol {
-
     EOF("eof"),
-    IDENTIFIER("Ident"),
-    NUMBER("Number"),
-    BEGIN("BEGIN"),
-    PROGRAM("PROGRAM"),
-    END("END"),
-    STRING("STRING"),
-    INT("INT"),
-    FLOAT("FLOAT"),
-    FUNCTION("FUNCTION"),
-    OBRACK('('),
-    CBRACK(')'),
-    QUOTA("\""),
+    IDENT("Ident"),
+    INTLITERAL("IntNumber"),
+    FLOATLITERAL("FloatNumber"),
+    STRINGLITERAL("StringLiteral"),
+    PROGRAM("program"),
+    BEGIN("begin"),
+    END("end"),
+    FUNCTION("function"),
+    READ("read"),
+    WRITE("write"),
+    IF("if"),
+    THEN("then"),
+    ELSE("else"),
+    ENDIF("endif"),
+    RETURN("return"),
+    FOR("for"),
+    ENDFOR("endfor"),
+    FLOAT("float"),
+    INT("int"),
+    VOID("void"),
+    STRING("string"),
     PLUS("+"),
     MINUS("-"),
     MULT("*"),
     DIV("/"),
+    EQUAL("="),
+    LT("<"),
+    GT(">"),
+    LPAR("("),
+    RPAR(")"),
     ASSIGN(":="),
     COMMA(","),
     SEMICOLON(";");
 
-    Symbol(String name) {
-        this.name = name;
-    }
+Symbol(String name) {
+    this.name = name;
+}
 
-    public String toString() {
-        return name;
-    }
+public String toString() {
+    return name;
+}
 
-    private String name;
+private String name;
 }
